9bab89cbb5e16d53a7b717b4ff036e8ee4feeb72
Marek <marekkozykowski@gmail.com>
2022-04-18 17:42:02 +0200

    secret key

--- django_championsite/settings.py
+++ django_championsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(kb97pxve&1rn12z$g*tb0s07oe)04ztw!9=j__*gso6y^vz_@'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
